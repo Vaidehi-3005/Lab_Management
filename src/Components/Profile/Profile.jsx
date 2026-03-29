0e7d2376c91114205fd09a2d4815dd1488af3810
Vaidehi-3005 <warambhevaidehi@gmail.com>
2026-03-29 23:19:50 +0530

    Added Profile  with profiledetails, edit and logout functionality

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Profile.css";
+import logo from "../../assets/logo.png"; 
+
+function Profile() {
+  const [open, setOpen] = useState(false);
+  const navigate =useNavigate();
+  return (
+    <div className="profile-container">
+      
+      <div className="profile" onClick={() => setOpen(!open)}>
+        <img src={logo} alt="profile" />
+      </div>
+
+     
+      {open && (
+        <div className="dropdown">
+          <div className="dropdown-header">
+            <img src={logo} alt="profile" />
+            <div>
+              <h4>admin</h4>
+              <p>admin@metacore.com</p>
+            </div>
+          </div>
+
+          <div className="dropdown-item"
+           onClick={() => {
+              navigate("/profiledetails");
+              setOpen(false); 
+            }}
+          >Profile Details</div>
+          <div className="dropdown-item">Security Settings</div>
+          <hr/>
+          <div className="dropdown-item">Logout</div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Profile;
