99cd02843122a7978240027176e3cdab24c7b3d6
Vaidehi-3005 <warambhevaidehi@gmail.com>
2026-03-29 21:38:06 +0530

    Sidebar Added

---
+++ src/Layouts/Sidebar/Sidebar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Sidebar.css";
+import logo from "../../assets/logo.png"; 
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-logo">
+        <img src={logo} alt="logo" />
+        <hr/>
+      </div>
+      <nav>
+        <NavLink to="/dashboard" className="nav-link">
+          Dashboard
+        </NavLink >
+        <NavLink to="/patients" className="nav-link">
+          Patients
+        </NavLink>
+        <NavLink to="/tests" className="nav-link">
+          Tests
+        </NavLink>
+        <NavLink to="/reports" className="nav-link">
+          Reports
+        </NavLink>
+      </nav>
+    </div>
+  );
+}
+
+export default Sidebar;
